860ff526a766ff1343aab08bc82c93e499d8b996
Devesh Kumar <devesh2027@gmail.com>
2018-11-27 22:58:05 +0530

    Final Commit For the Raw Project.

--- admin/adminchecker.php
+++ admin/adminchecker.php
@@ -1,19 +1,22 @@
 <?php
    // Script to check if the application has been properly installed.
 
-   $handle1=fopen("../inc/confirm.txt","r");
-   $handle2=fopen("../inc/config.php","r");
+   $handle1=fopen("../inc/confirm.txt", "r");
+   $handle2=fopen("../inc/config.php", "r");
    $handle3=fopen("adminconfig.php", "r");
+   $handle4=fopen("../inc/interconfig.php", "r")
 
    $text1=fread($handle1, filesize("../inc/confirm.txt"));
    $text2=fread($handle2, filesize("../inc/config.php"));
    $text3=fread($handle3, filesize("adminconfig.php"));
+   $text4=fread($handle4, filesize("../inc/interconfig.php"));
 
    fclose($handle1);
    fclose($handle2);
    fclose($handle3);
+   fclose($handle4);
 
-   if($text1=="0"||$text2=="0"||$text3=="0"){
+   if($text1=="0"||$text2=="0"||$text3=="0"||$text4=="0"){
    		header("refresh:0;url=../install");     // Redirect if any process might return false.
    		exit();
    }
--- inc/interconfig.php
+++ inc/interconfig.php
@@ -1,7 +1,1 @@
-<?php
-$maxops = 5;
-$footermessage = "<div class = 'header'>Footer</div>";
-$agreement = "This is to agree to the conditions that none of the Web Application's features shall be misused.
-
-Hence, I agree to sign this agreement.";
-?>
+0
